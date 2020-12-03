930ad6feaaeb2010b9bbfc627358716ebdd2f422
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-12-03 06:27:43 +0100

    Add functionality to replace search with results

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import './style.scss';
 import Weather from './weather';
 import displayInfo from './displayInfo';
 
-const searchForm = document.getElementById('search-city-form');
+const searchForm = document.querySelector('form');
+const page = document.querySelector('body');
+const header = document.querySelector('header');
+
 
 searchForm.addEventListener('submit', (e)=> {
      e.preventDefault();
+
      const cityName = document.querySelector('#city-name').value;
      
     if(cityName === ""){
       alert("please enter a valid city");
     }  
     else {
+     page.removeChild(header)
       const weather = new Weather(cityName);
       weather.getWeatherInfo()
         .then(results =>{
